47763e045e2326abc41485dd385a1d39b0c5eadd
Adam Fahie <adam.fahie@gmail.com>
2017-04-17 18:26:33 +0100

    Adam - Print out size of DataFrame

--- 00-naive-bayes-for-spam-detection/solution.py
+++ 00-naive-bayes-for-spam-detection/solution.py
@@ -4,4 +4,6 @@ sms_dataset = pandas.read_table(filepath_or_buffer='./SMSSpamCollection', sep='\
 
 sms_dataset.label = sms_dataset.label.map({'ham': 0, 'spam': 1})
 
+print("Total number of columns %d" % sms_dataset.shape[1])
+print("Total number of rows %d" % sms_dataset.shape[0])
 print(sms_dataset.head(5))
