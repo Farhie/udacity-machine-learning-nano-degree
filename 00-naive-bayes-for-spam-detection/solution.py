87cdce993a5af2986fdda0da257c3ad8045a3651
Adam Fahie <adam.fahie@gmail.com>
2017-04-17 18:18:19 +0100

    Adam - Mapping label column value to numeric value

--- 00-naive-bayes-for-spam-detection/solution.py
+++ 00-naive-bayes-for-spam-detection/solution.py
@@ -2,4 +2,6 @@ import pandas
 
 sms_dataset = pandas.read_table(filepath_or_buffer='./SMSSpamCollection', sep='\t', names=['label', 'sms_message'])
 
+sms_dataset.label = sms_dataset.label.map({'ham': 0, 'spam': 1})
+
 print(sms_dataset.head(5))
