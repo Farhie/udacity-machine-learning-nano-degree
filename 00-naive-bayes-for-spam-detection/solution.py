eb0cbe7b0e44702773f1468310b33487e6724a4d
Adam Fahie <adam.fahie@gmail.com>
2017-04-20 15:16:11 +0100

    Adam - Lower case all characters as first step of Bag of Words from scratch

--- 00-naive-bayes-for-spam-detection/solution.py
+++ 00-naive-bayes-for-spam-detection/solution.py
@@ -7,3 +7,9 @@ sms_dataset.label = sms_dataset.label.map({'ham': 0, 'spam': 1})
 print("Total number of columns %d" % sms_dataset.shape[1])
 print("Total number of rows %d" % sms_dataset.shape[0])
 print(sms_dataset.head(5))
+
+# Implementing bag of words from scratch
+
+sms_message_series = [sms_message.lower() for sms_message in sms_dataset.sms_message]
+
+print(sms_message_series)
